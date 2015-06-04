532295a1ce7af50ee06202f2a32d213528052265
Louis Hawkins <louishawkins@gmail.com>
2015-06-04 01:54:58 -0500

    update styling on pricing page

--- app/views/pricing.php
+++ app/views/pricing.php
@@ -3,39 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Louis Hawkins</title>
+    <script src="/js/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/css/pricing.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
-<body>
+<body style="background-color: white; font-family: 'Lato';">
     <div class="container">
         <div class="centered">
-            <div class="row">
+            <div class="row" style="margin-top:20px;">
                 <div class="pricing-cards">
                     <div class="col-sm-4 col-sm-offset-2">
-                        <div class="panel">
+                        <div class="panel card">
                             <div class="panel-heading"><h3 class="text-center">BASIC</h3></div>
                             <div class="panel-body">
                                 <div class="plan-cost">
-                                    <p><span class="dollar">$</span><span class="basic fee" price-month="25" price-year="19">19</span></p>
+                                    <p><span class="dollar">$</span><span class="fee">19</span></p>
                                     <p class="billing-plan">per user/month</p>
                                     <p>billed annually</p>
                                     <p></p>
                                 </div>
-                                <div class="xplan-description">
-                                    <p>billed monthly <span>$25</span></p>
+                                <div class="plan-description">
+                                    <p>billed monthly <span style="font-weight:700;">$25</span></p>
                                     <p></p>
                                 </div>
                                 <div class="plan-features">
-                                    <li><i class="icon-star"></i>Up to 50 Users</li>
-                                    <li><i class="icon-star"></i>CallCommand<sup>tm</sup> Dashboard</li>
-                                    <li><i class="icon-star"></i>Natural Language Processing</li>
-                                    <li><i class="icon-star"></i>Click-to-dial</li>
-                                    <li><i class="icon-star"></i>Real-time Analytics</li>
-                                    <li><i class="icon-star"></i>CRMconnect<sup>tm</sup> for SalesForce</li>
-                                    <li><i class="icon-star"></i>@mention syntax</li>
-                                    <li><i class="icon-star"></i>Reports sent to your inbox</li>
+                                    <ul>
+                                        <li>Up to 50 Users</li>
+                                        <li>CallCommand<sup>tm</sup> Dashboard</li>
+                                        <li>Natural Language Processing</li>
+                                        <li>Click-to-dial</li>
+                                        <li>Real-time Analytics</li>
+                                        <li>CRMconnect<sup>tm</sup> for SalesForce</li>
+                                        <li>@mention syntax</li>
+                                        <li>Reports sent to your inbox</li>
+                                    </ul>
                                 </div>
                                 <div class="choose-plan">
                                     <a href="#">Free Trial</a>
@@ -45,27 +50,27 @@
                     </div>
 
                     <div class="col-sm-4" style="margin-left: 25px;">
-                        <div class="panel">
+                        <div class="panel card">
                             <div class="panel-heading"><h3 class="text-center">PROFESSIONAL</h3></div>
                             <div class="panel-body">
                                 <div class="plan-cost">
-                                    <p><span class="dollar">$</span><span class="enterprise fee" price-month="49" price-year="39">39</span></p>
+                                    <p><span class="dollar">$</span><span class="fee">39</span></p>
                                     <p class="billing-plan">per user/month</p>
                                     <p>billed annually</p>
                                     <p></p></div>
-                                <div class="xplan-description">
-                                    <p>billed monthly <span>$49</span></p>
+                                <div class="plan-description">
+                                    <p>billed monthly <span style="font-weight:700;">$49</span></p>
                                     <p></p></div>
                                 <div class="plan-features">
                                     <ul>
-                                        <li><i class="icon-star"></i>Unlimited Users</li>
-                                        <li><i class="icon-star"></i>TeamCommand<sup>tm</sup> Dashboard</li>
-                                        <li><i class="icon-star"></i>Natural Language Processing</li>
-                                        <li><i class="icon-star"></i>Click-to-dial</li>
-                                        <li><i class="icon-star"></i>Real-time Analytics</li>
-                                        <li><i class="icon-star"></i>CRMconnect<sup>tm</sup> for SalesForce</li>
-                                        <li><i class="icon-star"></i>@mention syntax</li>
-                                        <li><i class="icon-star"></i>Reports sent to your inbox</li>
+                                        <li>Unlimited Users</li>
+                                        <li>TeamCommand<sup>tm</sup> Dashboard</li>
+                                        <li>Natural Language Processing</li>
+                                        <li>Click-to-dial</li>
+                                        <li>Real-time Analytics</li>
+                                        <li>CRMconnect<sup>tm</sup> for SalesForce</li>
+                                        <li>@mention syntax</li>
+                                        <li>Reports sent to your inbox</li>
                                     </ul>
                                 </div>
                                 <div class="choose-plan">
